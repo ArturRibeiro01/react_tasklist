336bd6e5b43549cd334e12d393d63e3cdba1a646
Artur Ribeiro <profissional.arturrc@gmail.com>
2021-07-28 00:01:43 -0300

    Configuração do Painel

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ import "./assets/App.css";
 import "./assets/index.css";
 
 class App extends Component {
+  constructor() {
+    this.notas = [];
+  }
+
+  createTask(title, message) {
+    console.log(`Uma nova nota foi criada ${title} ${message}`);
+  }
+
   render() {
     return (
       <section className="conteudo">
-        <FormCadastro />
-        <ListaDeNotas />
+        <FormCadastro createCard={this.createTask} />
+        <ListaDeNotas notas={[this.notas]} />
       </section>
     );
   }
